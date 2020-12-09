b1e091368a37de7826f857b91f2b3184bc3f9112
Abdul Rafay Modi <subjectalpha32@gmail.com>
2020-12-09 20:27:36 +0500

    A simple Android Date Picker using material design components

---
+++ app/src/main/java/com/example/datepicker/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.datepicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.date);
+        //Material Date Picker
+        MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
+        builder.setTitleText("Select a date");
+        MaterialDatePicker materialDatePicker = builder.build();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                materialDatePicker.show(getSupportFragmentManager(), "DATE_PICKER");
+            }
+        });
+        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                textView.setText("Selected date: " + materialDatePicker.getHeaderText());
+            }
+        });
+    }
+}
